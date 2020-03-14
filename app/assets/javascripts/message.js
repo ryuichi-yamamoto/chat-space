42896119c1503df009f1729092ca2ca9db18b475
ryuichi-yamamoto <yamamotoryuichi0802@gmail.com>
2020-03-14 20:15:14 +0900

    2020.03.14.20:15

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,40 +1,4 @@
 $(function(){ 
-  function buildHTML(message){
-    if ( message.image ) {
-      var html =
-        `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          </div>
-          <img src=${message.image} >
-        </div>`
-      return html;
-    } else {
-      var html =
-        `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
-  }
   var buildHTML = function(message) {
     if (message.content && message.image) {
       var html = `<div class="message" data-message-id= ${message.id} >
