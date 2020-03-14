94b5f9fe513af195df37157f1823744010679b61
ryuichi-yamamoto <yamamotoryuichi0802@gmail.com>
2020-03-14 19:35:43 +0900

    2020.03.14.19:35

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,18 +35,69 @@ $(function(){
       return html;
     };
   }
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id= ${message.id} >
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+          ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+          ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+          ${message.content}
+          </p>
+          <img src=" message.image " class="lower-message__image" >
+        </div>
+      </div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+          ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+          ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+          ${message.content}
+          </p>
+        </div>
+      </div>`
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id= message.id >
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            message.user_name
+          </div>
+          <div class="upper-message__date">
+            message.created_at
+          </div>
+        </div>
+        <div class="lower-message">
+          <img src=" message.image " class="lower-message__image" >
+        </div>
+      </div>`
+    };
+    return html;
+  };
   $('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
     url: url,
     type: "POST",
     data: formData,
     dataType: 'json',
     processData: false,
     contentType: false
-  })
+    })
     .done(function(data){
       var html = buildHTML(data);
       $('.chat-main__message-list').append(html);
@@ -60,5 +111,31 @@ $(function(){
       $('input[type="submit"]').prop('disabled',false);
     });
   })
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+
+      $('.chat-main__message-list').append(insertHTML);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+    }
+  })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
 });
+
 
