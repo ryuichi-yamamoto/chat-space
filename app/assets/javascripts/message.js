f0d59896f38b3d9689de0cd8de25f19dcaf26553
ryuichi-yamamoto <yamamotoryuichi0802@gmail.com>
2020-03-14 12:26:29 +0900

    2020.03.14.12:25jQuery.API

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,64 +1,64 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message">
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message">
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.chat-main__message-list').append(html);
-    $('form')[0].reset();
-    $('.chat-main .chat-main__message-list').animate({ scrollTop: $('.chat-main .chat-main__message-list')[0].scrollHeight});
+    if ( message.image ) {
+      var html =
+        `<div class="message">
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message">
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
   })
-  .fail(function(){
-   alert("エラー")
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__message-list').append(html);
+      $('form')[0].reset();
+      $('.chat-main .chat-main__message-list').animate({ scrollTop: $('.chat-main .chat-main__message-list')[0].scrollHeight});
+    })
+    .fail(function(){
+    alert("エラー")
+    })
+    .always(function() {
+      $('input[type="submit"]').prop('disabled',false);
+    });
   })
-  .always(function() {
-    $('input[type="submit"]').prop('disabled',false);
-  });
-})
 });
 
